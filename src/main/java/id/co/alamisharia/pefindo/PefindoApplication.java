5ba6a6a5f36ef544ae17cf88c383cf8dc39d06f1
Arief Luthfi Aulia <ariefluthfi.aulia@gmail.com>
2020-04-18 21:19:57 +0700

    Initial

---
+++ src/main/java/id/co/alamisharia/pefindo/PefindoApplication.java
@@ -0,0 +1,13 @@
+package id.co.alamisharia.pefindo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PefindoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PefindoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/alamisharia/pefindo/PefindoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.alamisharia.pefindo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PefindoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
